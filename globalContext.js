910b014fe48823d7364b89dd83ca801cee357675
rsm09 <161129741+rsm09@users.noreply.github.com>
2025-03-05 21:13:03 +0530

    Create globalContext.js

---
+++ globalContext.js
@@ -0,0 +1,20 @@
+let age=20;
+
+function displayAge(){
+  console.log(age)
+}
+
+displayAge()
+
+function changeAge(){
+  age++
+  console.log(age)
+}
+
+changeAge()
+
+console.log(age)
+
+/*20
+21
+21*/
