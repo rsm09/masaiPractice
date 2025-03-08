15b00666f9d88cf6a741e94be2efba586acd4bd2
rsm09 <161129741+rsm09@users.noreply.github.com>
2025-03-08 20:18:12 +0530

    Create bankAccount.js

---
+++ bankAccount.js
@@ -0,0 +1,28 @@
+function createBankAccount(balance){
+  return {
+    deposit: function(amount){
+      return balance= balance + amount
+    },
+    
+    withdraw: function(amount){
+      if(balance < amount){
+        return "Insufficient balance"
+      } else {
+        return balance= balance - amount
+      }
+    },
+      
+    getBalance: function(){
+      return balance
+    }
+      
+  }
+}
+
+const account = createBankAccount(100);
+
+console.log(account.deposit(50));
+
+console.log(account.withdraw(30)); 
+
+console.log(account.getBalance()); 
